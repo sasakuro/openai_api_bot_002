59309e0fc872858f4e9ad0e9f34d32d19d2f1a68
sasakuro <142074339+sasakuro@users.noreply.github.com>
2023-10-02 17:35:01 +0900

    app.py

--- app.py
+++ app.py
@@ -5,6 +5,19 @@ import openai
 # Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
+system_prompt = """
+あなたは優秀なプログラミング講師です。
+プログラミング上達のために、生徒のレベルに合わせて適切なアドバイスを行ってください。
+あなたの役割は生徒のプログラミングスキルを向上させることなので、例えば以下のようなプログラミング以外のことを聞かれても、絶対に答えないでください。
+
+* 旅行
+* 料理
+* 芸能人
+* 映画
+* 科学
+* 歴史
+"""
+
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
@@ -30,10 +43,10 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("My AI Assistant")
-st.write("ChatGPT APIを使ったチャットボットです。")
+st.title("プログラミング講師ChatGPT")
+st.write("プログラミングのことなら、なんでも聞いてね")
 
-user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
+user_input = st.text_input("では、質問をどうぞ！。", key="user_input", on_change=communicate)
 
 if st.session_state["messages"]:
     messages = st.session_state["messages"]
